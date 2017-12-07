aec15f79533d44e3ee94bb20626ed49ad87d4da5
SheepBeast <2982973480@qq.com>
2017-12-08 00:38:31 +0800

    优化home页的tab转场

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -27,36 +27,68 @@
 </template>
 
 <script>
-import find from "./find";
-// import recommend from './recommend'
+  import find from "./find";
 
-const recommend = resolve => require(["./recommend"], resolve),
-  event = resolve => require(["./event"], resolve);
+  const recommend = resolve => require(["./recommend"], resolve),
+    event = resolve => require(["./event"], resolve);
 
-export default {
-  name: "home",
-  data() {
-    return {
-      currentView: "find",
-      tabs: [
-        { en: "find", zh: "发现" },
-        { en: "recommend", zh: "推荐" },
-        { en: "event", zh: "动态" }
-      ],
-      tabTransition: null
-    };
-  },
-  components: {
-    find,
-    recommend,
-    event
-  },
-  computed: Vuex.mapGetters("musicPlayer", ["playing", "currentPlays"]),
-  methods: {
-    viewChange({ next, prev }) {
-      this.tabTransition = `tab-slide-${next > prev ? "left" : "right"}`;
-      this.currentView = this.tabs[next].en;
+  export default {
+    name: "home",
+    data() {
+      return {
+        currentView: "find",
+        tabs: [
+          { en: "find", zh: "发现", initialized: true },
+          {
+            en: "recommend",
+            zh: "推荐",
+            initialized: false,
+            preload({ dispatch }, payload) {
+              dispatch('recommend/getRecommend', payload)
+            }
+          },
+          {
+            en: "event",
+            zh: "动态",
+            initialized: false,
+            preload({ dispatch }, payload) {
+              dispatch("event/getEvent", payload);
+            }
+          }
+        ],
+        tabTransition: null
+      };
+    },
+    components: {
+      find,
+      recommend,
+      event
+    },
+    computed: Vuex.mapGetters("musicPlayer", ["playing", "currentPlays"]),
+    methods: {
+      /**
+       * 切换至recommend，event页面时需要预加载数据
+       * next 即将切换后的tab索引
+       * prev 切换前的tab索引
+       * fire tab组件内部的promise实例的resolve回调，不传值时或传值结果的布尔值为true时切换tab，传值结果的布尔值为false时中止切换
+      */
+      viewChange({ next, prev, fire }) {
+        let comp = this.tabs[next], self = this,
+          done = function () {
+            Vue.$loadingTip.hide()
+            fire()
+            self.currentView = self.tabs[next].en;
+          }
+        this.tabTransition = `tab-slide-${next > prev ? "left" : "right"}`;
+
+        if (!comp.initialized) {
+          Vue.$loadingTip.show()
+          comp.preload(this.$store, { done })
+          comp.initialized = true
+        } else {
+          done()
+        }
+      }
     }
-  }
-};
+  };
 </script>
--- src/views/home/recommend.vue
+++ src/views/home/recommend.vue
@@ -1,34 +1,29 @@
 <template>
-  <page-content style="padding-top: 0">
-    <template v-show="recommend">
-      <div class="recommend">
-        <section class="banner" v-if="banner">
-          <router-link :to="{name: 'musicPlayer', query: {id: banner.sid}}" tag="img" :src="banner.picUrl"></router-link>
-        </section>
-        <list>
-          <list-item v-for="(tl, idx) in recommend" :key="tl.id">
-            <span class="index">{{idx + 1}}</span>
-            <router-link :to="{name: 'musicPlayer', query: {id: tl.id}}" tag="div" class="list-item-holder dense">
-              <list-item-inner>
-                <div class="primary-title f-elpsl-1">{{tl.name}}</div>
-                <div class="third-title f-elpsl-1">{{tl.artists}} - {{tl.album}}</div>
-              </list-item-inner>
-              <icon ligature="play_circle_outline" class="icon-play"></icon>
-            </router-link>
-          </list-item>
-        </list>
-      </div>
-    </template>
-    <loading-page v-show="!recommend"></loading-page>
+  <page-content style="padding-top: 0" v-if="recommend">
+    <div class="recommend">
+      <section class="banner" v-if="banner">
+        <router-link :to="{name: 'musicPlayer', query: {id: banner.sid}}" tag="img" :src="banner.picUrl"></router-link>
+      </section>
+      <list>
+        <list-item v-for="(tl, idx) in recommend" :key="tl.id">
+          <span class="index">{{idx + 1}}</span>
+          <router-link :to="{name: 'musicPlayer', query: {id: tl.id}}" tag="div" class="list-item-holder dense">
+            <list-item-inner>
+              <div class="primary-title f-elpsl-1">{{tl.name}}</div>
+              <div class="third-title f-elpsl-1">{{tl.artists}} - {{tl.album}}</div>
+            </list-item-inner>
+            <icon ligature="play_circle_outline" class="icon-play"></icon>
+          </router-link>
+        </list-item>
+      </list>
+    </div>
   </page-content>
+  <loading-page v-else></loading-page>
 </template>
 
 <script>
   export default {
     name: "recommend",
-    computed: Vuex.mapGetters("recommend", ["recommend", "banner"]),
-    beforeCreate() {
-      this.$store.dispatch("recommend/getRecommend");
-    }
+    computed: Vuex.mapGetters("recommend", ["recommend", "banner"])
   };
 </script>
